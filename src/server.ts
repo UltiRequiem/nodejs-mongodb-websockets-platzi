1f0b15d72c61301c70e73891aac2f2f51c6ec7b0
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-24 11:00:01 -0500

    :page_facing_up: Eslint Config

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello World");
+app.get("/", (_request, response) => {
+  response.send({ message: "Hello" });
 });
 
 app.listen(PORT, () => {
